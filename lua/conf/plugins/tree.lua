64817cc419256a1c614951ab981f0f25b62123ce
AbdelrahmanAyman0011 <abdelrahmanayman0011@gmail.com>
2026-08-30 00:20:14 +0300

    Fix: remove nvim-tree <C-t> mapping to not conflict with toggleterm

--- lua/conf/plugins/tree.lua
+++ lua/conf/plugins/tree.lua
@@ -6,7 +6,18 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+
+      -- apply default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- remove the <C-t> mapping so toggleterm can use it
+      vim.keymap.del("n", "<C-t>", { buffer = bufnr })
+    end
+
     require("nvim-tree").setup {
+      on_attach = my_on_attach,
       sync_root_with_cwd = true,
       respect_buf_cwd = true,
       update_focused_file = {
